49f023b887a36dbf8d34c53e232adb536c333e9b
pham trung dong <dongtrung55@gmail.com>
2019-03-28 10:38:26 +0700

    update

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php
 //load model
 include "model/Repo.php";
-//load kết nối
-include_once "config/Config.php";
-
-//lấy giá trị controller truyền từ url
-$controller = isset($_GET["controller"]) ? $_GET["controller"] : "";
-if ($controller != "") {
-    $controller = "controller/Controller_$controller.php";
-} else {
-    $controller = "controller/Controller_CBGV.php";
-}
-include "view/Master.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QL Lương</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.css"/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="public/css/Styles.css">
+    <script>
+        $(document).ready(function () {
+            $('#listcbgv').DataTable({
+                responsive: true
+            });
+        });
+    </script>
+</head>
+<body>
+<div class="container">
+    <?php include "controller/Controller_GV.php"; ?>
+</div>
+</body>
+</html>
