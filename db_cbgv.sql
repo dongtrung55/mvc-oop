a16b25ec11e52e8286ec5d319334d2b3b7cb0b4f
pham trung dong <dongtrung55@gmail.com>
2019-03-28 10:43:18 +0700

    update

--- db_cbgv.sql
+++ db_cbgv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 27, 2019 lúc 04:44 AM
--- Phiên bản máy phục vụ: 10.1.34-MariaDB
--- Phiên bản PHP: 7.2.7
+-- Thời gian đã tạo: Th3 28, 2019 lúc 04:42 AM
+-- Phiên bản máy phục vụ: 10.1.38-MariaDB
+-- Phiên bản PHP: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,28 +29,27 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tblcbgv` (
-  `cbgv_id` int(11) NOT NULL,
-  `cbgv_name` varchar(30) NOT NULL,
-  `cbgv_birthday` date NOT NULL,
-  `cbgv_address` varchar(300) NOT NULL,
-  `cbgv_luongcung` double NOT NULL,
-  `cbgv_luongthuong` double NOT NULL,
-  `cbgv_luongphat` double NOT NULL,
-  `cbgv_luonglinhthuc` double NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `birthDay` date NOT NULL,
+  `address` varchar(300) NOT NULL,
+  `luongCung` double NOT NULL,
+  `luongThuong` double NOT NULL,
+  `luongPhat` double NOT NULL,
+  `luongLinhThuc` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `tblcbgv`
 --
 
-INSERT INTO `tblcbgv` (`cbgv_id`, `cbgv_name`, `cbgv_birthday`, `cbgv_address`, `cbgv_luongcung`, `cbgv_luongthuong`, `cbgv_luongphat`, `cbgv_luonglinhthuc`) VALUES
+INSERT INTO `tblcbgv` (`id`, `name`, `birthDay`, `address`, `luongCung`, `luongThuong`, `luongPhat`, `luongLinhThuc`) VALUES
 (1, 'Phạm Trung Đông', '1997-09-05', 'Tân Cương - Vĩnh Tường - Vĩnh Phúc', 6000000, 1000000, 100000, 6900000),
 (2, 'Nguyễn Thị Lan', '1996-11-22', 'Lý Nhân - Hà Nam', 6000000, 1000000, 100000, 6900000),
 (3, 'Vũ Thị Loan', '1995-08-16', 'Ninh Bình', 5000000, 1000000, 100000, 5900000),
 (4, 'Bùi Trung Kiên', '1993-05-10', 'Hà Nội, Việt Nam', 10000000, 2000000, 500000, 11500000),
 (5, 'Trần Trịnh Quỳnh Lâm', '1997-01-01', 'Hà Nội, Việt Nam', 5000000, 1000000, 500000, 5500000),
-(6, 'Đào Văn Huy', '1995-01-01', 'Hà Nội, Việt Nam', 5000000, 500000, 200000, 5300000),
-(7, 'Nguyễn Thị Thu Thảo', '1997-01-01', 'Đan Phượng, Hà Nội, Việt Nam', 5000000, 1000000, 700000, 5300000);
+(7, 'Nguyễn Thị Thu Thảo', '2019-03-15', 'Đan Phượng, Hà Nội, Việt Nam', 5000000, 100000, 30000, 5070000);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -60,7 +59,7 @@ INSERT INTO `tblcbgv` (`cbgv_id`, `cbgv_name`, `cbgv_birthday`, `cbgv_address`, 
 -- Chỉ mục cho bảng `tblcbgv`
 --
 ALTER TABLE `tblcbgv`
-  ADD PRIMARY KEY (`cbgv_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
@@ -70,7 +69,7 @@ ALTER TABLE `tblcbgv`
 -- AUTO_INCREMENT cho bảng `tblcbgv`
 --
 ALTER TABLE `tblcbgv`
-  MODIFY `cbgv_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
