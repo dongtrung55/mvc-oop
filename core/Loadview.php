bb02b206801bf4dd3d5729c32212342dcc22372b
pham trung dong <dongtrung55@gmail.com>
2019-03-29 15:19:40 +0700

    update

--- core/Loadview.php
+++ core/Loadview.php
@@ -5,7 +5,7 @@ class LoadView
     protected function loadV($view = '', $data = [], $id = '', $form_action = '')
     {
         //kiểm tra xem file tồn tại không
-        if(file_exists(VIEW_PATH . $view . '.php')){
+        if (file_exists(VIEW_PATH . $view . '.php')) {
             //Nếu tồn tại thì nhúng file vào.
             include_once VIEW_PATH . $view . '.php';
         }
